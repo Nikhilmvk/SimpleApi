621cde153871d025cb1ff1e9ca2920bbf17da7f6
Nikhil Anand <112142828+Nikhilmvk@users.noreply.github.com>
2026-02-03 10:02:14 +0530

    Initialize ASP.NET Core application with Swagger

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using SimpleApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
