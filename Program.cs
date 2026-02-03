ed7c8bbe334f5ee6ef9b5d6e1d3128416812e73d
Nikhil Anand <112142828+Nikhilmvk@users.noreply.github.com>
2026-02-03 10:54:10 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,30 @@
 using Microsoft.EntityFrameworkCore;
 using SimpleApi.Data;
+using SimpleApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
-builder.Services.AddControllers();
+// Add services to the container
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+
+// Add Swagger services
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();  // <-- important
 
 var app = builder.Build();
 
-// Middleware
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseHttpsRedirection();
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();           // <-- important
+    app.UseSwaggerUI();         // <-- important
+}
 
+app.UseHttpsRedirection();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
