637c1f9937c172057bb528c2531f75e77b8f53fc
Nikhil Anand <112142828+Nikhilmvk@users.noreply.github.com>
2026-02-03 10:37:03 +0530

    Update Student.cs

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,8 +1,10 @@
+// C:\Users\Test\SimpleApi\Models\Student.cs
 namespace SimpleApi.Models
 {
     public class Student
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; }   // <-- Warning CS8618
     }
 }
+
